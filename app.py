40c5475ab5684263693244353505016a4c91969d
pk <piotr.m.klis@gmail.com>
2022-02-18 19:55:25 +0100

    vaults history

--- app.py
+++ app.py
@@ -15,6 +15,11 @@ from datetime import datetime
 import atexit
 from flask_talisman import Talisman
 from flask_wtf.csrf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from sqlalchemy.dialects import registry
+
+import os
 
 from config import SECRET
 from connectors.sf import sf, sf_disconnect
@@ -22,7 +27,9 @@ from views.main_view import main_page_view, main_page_data
 from views.vault_view import vault_page_view, vault_page_data
 from views.collateral_view import collateral_page_view, collateral_page_data
 from views.owner_view import owner_page_view, owner_page_data
+from views.history_view import history_page_view
 
+registry.register("snowflake", "snowflake.sqlalchemy", "dialect")
 
 app = Flask(__name__)
 app.secret_key = SECRET
@@ -30,31 +37,97 @@ app.config["JSON_SORT_KEYS"] = False
 app.config["DEBUG"] = True
 csrf = CSRFProtect(app)
 
-
-SELF = "'self'"
-UNSAFE_INLINE = "'unsafe-inline'"
-UNSAFE_EVAL = "'unsafe-eval'"
-talisman = Talisman(
-    app,
-    force_https=False,
-    content_security_policy={
-        "default-src": [SELF],
-        "img-src": ["*", "data:"],
-        "script-src": [
-            SELF,
-            UNSAFE_EVAL,
-            UNSAFE_INLINE,
-            "https://cdn.plot.ly/",
-            "https://cdn.datatables.net/",
-            "https://cdnjs.cloudflare.com/",
-            "https://code.jquery.com/",
-        ],
-        "style-src": [SELF, UNSAFE_INLINE, "https://cdnjs.cloudflare.com/"],
-        "font-src": [SELF, "https://cdnjs.cloudflare.com/"],
+connect_url = sqlalchemy.engine.url.URL(
+    "snowflake",
+    username=os.getenv("SNOWFLAKE_USER"),
+    password=os.getenv("SNOWFLAKE_PASS"),
+    host=os.getenv("SNOWFLAKE_ACCOUNT"),
+    query={
+        "database": os.getenv("SNOWFLAKE_DATABASE"),
+        "role": os.getenv("SNOWFLAKE_ROLE"),
+        "warehouse": os.getenv("SNOWFLAKE_WAREHOUSE"),
     },
-    content_security_policy_nonce_in=["script-src"],
-    feature_policy={"geolocation": "'none'"},
 )
+
+app.config['SQLALCHEMY_DATABASE_URI'] = connect_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# SELF = "'self'"
+# UNSAFE_INLINE = "'unsafe-inline'"
+# UNSAFE_EVAL = "'unsafe-eval'"
+# talisman = Talisman(
+#     app,
+#     force_https=False,
+#     content_security_policy={
+#         "default-src": [SELF],
+#         "img-src": ["*", "data:"],
+#         "script-src": [
+#             SELF,
+#             UNSAFE_EVAL,
+#             UNSAFE_INLINE,
+#             "https://cdn.plot.ly/",
+#             "https://cdn.datatables.net/",
+#             "https://cdnjs.cloudflare.com/",
+#             "https://code.jquery.com/",
+#             "https://cdn.jsdelivr.net/",
+#             "https://cdn.datatables.net/"
+
+#         ],
+#         "style-src": [SELF, UNSAFE_INLINE, "https://cdn.datatables.net/", "https://cdnjs.cloudflare.com/", "https://cdn.jsdelivr.net"],
+#         "font-src": [SELF, "https://cdnjs.cloudflare.com/"],
+#     },
+#     content_security_policy_nonce_in=["script-src"],
+#     feature_policy={"geolocation": "'none'"},
+# )
+
+class History(db.Model):
+
+    __tablename__ = 'vaults'
+
+    day = db.Column(db.DateTime)
+    vault = db.Column(db.String)
+    ilk = db.Column(db.String)
+    collateral_eod = db.Column(db.Float)
+    principal_eod = db.Column(db.Float)
+    debt_eod = db.Column(db.Float)
+    fees_eod = db.Column(db.Float)
+    withdraw = db.Column(db.Float)
+    deposit = db.Column(db.Float)
+    principal_generate = db.Column(db.Float)
+    principal_payback = db.Column(db.Float)
+    debt_generate = db.Column(db.Float)
+    debt_payback = db.Column(db.Float)
+    fees = db.Column(db.Float)
+
+    def to_dict(self):
+        return {
+            'day' : self.day,
+            'vault' : self.vault, 
+            'ilk' : self.ilk,
+            'collateral_eod' : self.collateral_eod,
+            'principal_eod' : self.principal_eod,
+            'debt_eod' : self.debt_eod,
+            'fees_eod' : self.fees_eod,
+            'withdraw' : self.withdraw,
+            'deposit' : self.deposit,
+            'principal_generate' : self.principal_generate,
+            'principal_payback' : self.principal_payback,
+            'debt_generate' : self.debt_generate,
+            'debt_payback' : self.debt_payback,
+            'fees' : self.fees,
+        }
+    
+    __table_args__ = {"schema": "maker.history"}
+    __mapper_args__ = {
+        "primary_key": [
+            day,
+            vault,
+            ilk
+        ]
+    }
+
+db.create_all()
 
 
 # HTML endpoints -------------------------------------------
@@ -95,6 +168,11 @@ def tos_page():
     return render_template("tos.html", refresh=datetime.utcnow())
 
 
+@app.route("/history")
+def history_page():
+    return history_page_view(sf)
+
+
 # DATA endpoints -------------------------------------------
 
 
@@ -120,6 +198,53 @@ def get_collateral_page_data(collateral_id):
 def get_owner_page_data(owner_id):
     dataset = owner_page_data(sf, owner_id)
     return jsonify(dataset)
+
+
+@app.route("/data/history", methods=["GET"])
+def data():
+    query = History.query
+
+    # search filter
+    search = request.args.get('search[value]')
+    if search:
+        query = query.filter(db.or_(
+            History.day.like(f'%{search}%'),
+            History.vault.like(f'%{search}%'),
+            History.ilk.like(f'%{search}%')
+        ))
+    total_filtered = query.count()
+
+    # sorting
+    order = []
+    i = 0
+    while True:
+        col_index = request.args.get(f'order[{i}][column]')
+        if col_index is None:
+            break
+        col_name = request.args.get(f'columns[{col_index}][data]')
+        if col_name not in ['day', 'vault', 'ilk']:
+            col_name = 'day'
+        descending = request.args.get(f'order[{i}][dir]') == 'desc'
+        col = getattr(History, col_name)
+        if descending:
+            col = col.desc()
+        order.append(col)
+        i += 1
+    if order:
+        query = query.order_by(*order)
+
+    # pagination
+    start = request.args.get('start', type=int)
+    length = request.args.get('length', type=int)
+    query = query.offset(start).limit(length)
+
+    # response
+    return {
+        'data': [record.to_dict() for record in query],
+        'recordsFiltered': total_filtered,
+        'recordsTotal': History.query.count(),
+        'draw': request.args.get('draw', type=int),
+    }
 
 
 # cleanup tasks
